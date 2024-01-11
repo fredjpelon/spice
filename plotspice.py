51de9a05295f93999fc90e04b63c780034630496
fredjpelon <fred.pelon@gmail.com>
2024-01-11 10:13:13 -0700

    ngspice first repo
    an ngspice playground instantiation

---
+++ plotspice.py
@@ -0,0 +1,9 @@
+import plotext as plt
+import numpy as np
+
+y = plt.sin() # sinusoidal signal
+plt.plot(y)
+plt.title("FFT Magnitude Plot")
+plt.xlabel("Frequency Line")
+plt.ylabel("Magnitude")
+plt.show()
